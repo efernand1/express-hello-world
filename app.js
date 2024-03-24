fba979397175413d7296b1fb24b863f9b1bc962f
Erich F. <erichdevelopment@gmail.com>
2024-03-24 15:52:54 -0400

    Adding health check path endpoint

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.type("html").send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Add a new route for the health endpoint
+
+app.get("/health", (req, res) => {
+  res.sendStatus(200);
+});
+
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
@@ -58,4 +66,4 @@ const html = `
     </section>
   </body>
 </html>
-`
+`;
